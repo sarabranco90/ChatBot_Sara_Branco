4bbb3e8b92719e222f01140539a40b356f8fb886
Sara Branco <sarabranco1990@gmail.com>
2025-01-21 13:55:26 +0000

    Nome + Apelido
    Adicionei Nome + Apelido

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 nome = input("Qual é o teu nome? ")
 
-idade = int(input("Quantos anos tens?"))
-print(f"Olá {nome}! Tu tens {idade} anos. ")                
+idade = int(input("Quantos anos tens? "))
+print(f"Olá {nome}! Tu tens {idade} anos. ")
+
+apelido = input("Qual é o teu apelido? ")
+print(f"Então o teu nome completo é {nome} {apelido}.")
+           
