977ddc368f92f39b3957553b402277286b5a8a2c
Sara Branco <sarabranco1990@gmail.com>
2025-01-23 16:21:39 +0000

    1º ponto concluído

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 def obter_resposta(texto: str) -> str:
     comando: str = texto.lower()
 
+"""
     if comando in ('olá', 'boa tarde', 'bom dia'):
         return 'Olá tudo bem!'
     if comando == 'como estás':
@@ -18,10 +19,9 @@ def obter_resposta(texto: str) -> str:
         return f'São: {datetime.now():%H:%M} horas'
     if 'data' in comando:
         return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
-
+"""
     return f'Desculpa, não entendi a questão! {texto}'
 
-"""
     respostas = {
         ('olá', 'boa tarde', 'bom dia'): 'Olá tudo bem!',
         'como estás': 'Estou bem, obrigado!',
@@ -46,7 +46,7 @@ def obter_resposta(texto: str) -> str:
         return f'Hoje é dia: {datetime.now():%d-%m-%Y}'
 
     return f'Desculpa, não entendi a questão! {texto}'
-"""
+
 
 
 def chat() -> None:
